49390b93249aa5ef8f7538136392fcffc2aa013e
megabite.pet <karen.o.sellgren@gmail.com>
2025-04-10 11:36:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+
+document.querySelector('.language-toggle').addEventListener('click', function (e) {
+    e.stopPropagation();
+    document.querySelector('.language-menu').classList.toggle('show');
+});
+
+document.addEventListener('click', function () {
+    document.querySelector('.language-menu').classList.remove('show');
+});
+
